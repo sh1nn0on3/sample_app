99491d993d14d46cc68171bed833e5396dedda8c
sh1nn0on3 <sanogaku31@gmail.com>
2024-04-03 10:02:31 +0700

    route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+    def full_title(page_title = '')
+      base_title = "Your Base Title Here" 
+      if page_title.empty?
+        base_title
+      else
+        "#{page_title} | #{base_title}"
+      end
+    end
+  end
