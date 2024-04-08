81300ba72513c655cf59db87636441302236fb02
sh1nn0on3 <115028594+sh1nn0on3@users.noreply.github.com>
2024-04-08 11:23:51 +0700

    ..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates :name,  presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
@@ -44,5 +44,5 @@ class User < ApplicationRecord
     def forget
       update_attribute(:remember_digest, nil)
     end
-  end
+end
   
