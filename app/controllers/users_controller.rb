174cb0db59b3f067d3e08d9946d458e44038e42b
sh1nn0on3 <115028594+sh1nn0on3@users.noreply.github.com>
2024-04-08 14:57:32 +0700

    i18n

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = I18n.t("Controller.User.Create")
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
