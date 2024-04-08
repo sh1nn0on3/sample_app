63278cf3967bf796e691995b44e376c8b5e554c5
sh1nn0on3 <sanogaku31@gmail.com>
2024-04-08 09:24:57 +0700

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_05_090814) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_08_014704) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,9 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_05_090814) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
-    t.string "activation_digest"
-    t.boolean "activated", default: false
+    t.boolean "activated"
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
