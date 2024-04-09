7f5f959a67dfbe61fdaf6ce0143961eafb151c03
sh1nn0on3 <115028594+sh1nn0on3@users.noreply.github.com>
2024-04-09 16:22:56 +0700

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+  validates :name,  presence: true, length: { maximum: Settings.validation.name.maximum }
+  validates :email, presence: true, length: { maximum: Settings.validation.email.maximum },
+                    format: { with: Settings.validation.VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
