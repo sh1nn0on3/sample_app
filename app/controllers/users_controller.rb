4e3afd4d971a405071536bcbeec3cf93b77ee493
sh1nn0on3 <115028594+sh1nn0on3@users.noreply.github.com>
2024-04-09 09:03:40 +0700

    ....

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :set_user, only: %i[show edit update destroy]
   before_action :logged_in_user, only: %i[index edit update destroy]
-  before_action :correct_user, only: %i[edit, update]
+  before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
   
   def set_user
